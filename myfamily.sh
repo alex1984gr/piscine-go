3b6e74cde930e09303bad44cf0be74920c19687e
agaliand <alexandrosgaliandras@yahoo.com>
2025-09-01 18:19:30 +0300

    myfamily.sh

--- myfamily.sh
+++ myfamily.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-curl -s https://platform.zone01.gr/assets/superhero/all.json \
+if [ -z "$HERI_ID" ]; then
+echo "error : HERO_ID is not set."
+if curl -s https://platform.zone01.gr/assets/superhero/all.json \
 | jq -r ".[] | select(.id == $HERO_ID) | .relatives"

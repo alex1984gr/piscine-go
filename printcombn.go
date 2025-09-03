6c15df98311d4b659e53cdbc2bcfeab2bf8a0217
agaliand <alexandrosgaliandras.com>
2025-09-03 20:12:14 +0300

    printcombn.go

--- printcombn.go
+++ printcombn.go
@@ -8,25 +8,34 @@ func PrintCombN(n int) {
 	}
 
 	comb := make([]int, n)
-	generateComb(0, 0, n, comb, true)
+	generateComb(0, 0, n, comb)
 	z01.PrintRune('\n')
 }
 
-func generateComb(pos int, start int, n int, comb []int, first bool) {
+func generateComb(pos int, start int, n int, comb []int) {
 	if pos == n {
-		if !first {
-			z01.PrintRune(',')
-			z01.PrintRune(' ')
-		}
 		for i := 0; i < n; i++ {
 			z01.PrintRune(rune('0' + comb[i]))
+		}
+
+		if !isLast(comb, n) {
+			z01.PrintRune(',')
+			z01.PrintRune(' ')
 		}
 		return
 	}
 
 	for i := start; i <= 9-(n-pos); i++ {
 		comb[pos] = i
-		generateComb(pos+1, i+1, n, comb, first && pos == 0 && i == 0)
-		first = false
+		generateComb(pos+1, i+1, n, comb)
 	}
+}
+
+func isLast(comb []int, n int) bool {
+	for i := 0; i < n; i++ {
+		if comb[i] != 10-n+i {
+			return false
+		}
+	}
+	return true
 }

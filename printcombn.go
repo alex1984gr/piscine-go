80ca118ed84f6c81d2f2bf069897325f8254180e
agaliand <alexandrosgaliandras.com>
2025-09-03 19:21:58 +0300

    printcombn.go

---
+++ printcombn.go
@@ -0,0 +1,32 @@
+package student
+
+import "github.com/01-edu/z01"
+
+func PrintCombN(n int) {
+	if n <= 0 || n >= 10 {
+		return
+	}
+
+	comb := make([]int, n)
+	generateComb(0, 0, n, comb)
+	z01.PrintRune('\n')
+}
+
+func generateComb(pos int, start int, n int, comb []int) {
+	if pos == n {
+		for i := 0; i < n; i++ {
+			z01.PrintRune(rune('0' + comb[i]))
+		}
+
+		if !(comb[0] == 10-n && comb[n-1] == 9) {
+			z01.PrintRune(',')
+			z01.PrintRune(' ')
+		}
+		return
+	}
+
+	for i := start; i <= 9-(n-pos); i++ {
+		comb[pos] = i
+		generateComb(pos+1, i+1, n, comb)
+	}
+}

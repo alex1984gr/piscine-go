251eeceb34b2a74cc45a8463d00924e594c038a6
agaliand <alexandrosgaliandras@yahoo.com>
2025-09-04 17:04:54 +0300

    atoi.go

--- atoi.go
+++ atoi.go
@@ -1,16 +1,29 @@
 package student
 
 func Atoi(s string) int {
-	result := 0
 	if len(s) == 0 {
 		return 0
 	}
 
-	for _, r := range s {
+	neg := false
+	start := 0
+	if s[0] == '-' {
+		neg = true
+		start = 1
+	}
+
+	result := 0
+	for i := start; i < len(s); i++ {
+		r := s[i]
 		if r < '0' || r > '9' {
 			return 0
 		}
 		result = result*10 + int(r-'0')
 	}
+
+	if neg {
+		result = -result
+	}
+
 	return result
 }

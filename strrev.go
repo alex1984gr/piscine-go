2e6386eadeff6dde52cf4d60fd98d00ca63d1378
agaliand <alexandrosgaliandras@yahoo.com>
2025-09-04 16:02:22 +0300

    strrev.go

--- strrev.go
+++ strrev.go
@@ -1,8 +1,8 @@
-package student 
+package student
 
 func StrRev(s string) string {
 	runes := []rune(s)
-	for i, j := 0; len(runes)-1; i < j; i, j =i+1, j-1{
+	for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
 		runes[i], runes[j] = runes[j], runes[i]
 	}
 	return string(runes)

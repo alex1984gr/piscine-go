33f734247229ec2e243103f46d3a33b6eed789c2
agaliand <alexandrosgaliandras@yahoo.com>
2025-09-08 17:08:56 +0300

    recursivepower.go

--- recursivepower.go
+++ recursivepower.go
@@ -1,10 +1,10 @@
 package student
 
 func RecursivePower(nb int, power int) int {
-	if power < 0 {
+	if power < 0 || nb > 20 {
 		return 0
 	}
-	if power == 1 {
+	if power == 0 {
 		return 1
 	}
 	return nb * RecursivePower(nb, power-1)

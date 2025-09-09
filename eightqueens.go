d0e1b468063c5fee199c4def9355348da9b1fa5b
agaliand <alexandrosgaliandras@yahooo.com>
2025-09-09 12:18:01 +0300

    eightqueens.go

--- eightqueens.go
+++ eightqueens.go
@@ -20,15 +20,17 @@ func solve(j int, board *[8]int) {
 		}
 	}
 }
+
 func IsSafe(j, i int, board *[8]int) bool {
 	for c := 0; c < j; c++ {
 		r := board[c]
-		if r == i || abs(i-i) == abs(c-j) {
+		if r == i || abs(c-i) == abs(c-j) {
 			return false
 		}
 	}
 	return true
 }
+
 func abs(x int) int {
 	if x < 0 {
 		return -x

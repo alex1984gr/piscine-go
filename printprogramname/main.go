bb3ead9db139860743aa553f60252fe654835394
agaliand <alexandrosgaliandras@yahoo.com>
2025-09-10 13:03:24 +0300

    printprogramname

---
+++ printprogramname/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"os"
+
+	"github.com/01-edu/z01"
+)
+
+func main() {
+	args := os.Args
+	program := args[0]
+	lastSlash := -1
+	for i, c := range program {
+		if c == '/' {
+			lastSlash = i
+		}
+	}
+	name := program[lastSlash+1:]
+	for _, c := range name {
+		z01.PrintRune(c)
+	}
+}

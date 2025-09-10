6200c8b6f45f41ea5b89bbf325a55224862e5eb3
ALEXANDROS GALIANDRAS <alexandrosgaliandras@yahoo.com>
2025-09-10 16:53:46 +0300

    main.go

--- sortparams/main.go
+++ sortparams/main.go
@@ -1,14 +1,25 @@
 package main
 
-import "os"
+import (
+	"os"
+
+	"github.com/01-edu/z01"
+)
 
 func main() {
 	args := os.Args[1:]
-	for i := 0; i < len(args)-1; i++ {
-		for j := i + 1; j < len(args); j++ {
-			if args[i] > args[j] {
-				args[i], args[j] = args[j], args[i]
+	n := len(args)
+	for i := 0; i < n-1; i++ {
+		for j := 0; j < n-1-i; j++ {
+			if args[j] > args[j+1] {
+				args[j], args[j+1] = args[j+1], args[j]
 			}
 		}
+	}
+	for _, arg := range args {
+		for _, c := range arg {
+			z01.PrintRune(c)
+		}
+		z01.PrintRune('\n')
 	}
 }

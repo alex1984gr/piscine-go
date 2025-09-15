751d36aebb36b8d6b39e1ad5f9a9e8bd2631b268
ALEXANDROS GALIANDRAS <alexandrosgaliandras@yahoo.com>
2025-09-15 23:51:34 +0300

    displayfile

---
+++ displayfile/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+)
+
+func main() {
+	args := os.Args[1:]
+	if len(args) == 0 {
+		fmt.Println("File name missing")
+		return
+	}
+	if len(args) > 1 {
+		fmt.Print("To many arguments")
+		return
+	}
+	file, err := os.Open(args[0])
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+	defer file.Close()
+	if _, err := io.Copy(os.Stdout, file); err != nil {
+		fmt.Println(err.Error())
+	}
+}

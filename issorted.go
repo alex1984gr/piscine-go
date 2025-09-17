cf378b66e6b3501ad74d8de0361dea1ef8f387ef
ALEXANDROS GALIANDRAS <alexandrosgaliandras@yahoo.com>
2025-09-18 01:19:39 +0300

    issorted.go

---
+++ issorted.go
@@ -0,0 +1,20 @@
+package student
+
+func IsSorted(f func(a, b int) int, a []int) bool {
+	if len(a) <= 1 {
+		return true
+	}
+	increasing := true
+	decreasing := true
+	for i := 0; i < len(a)-1; i++ {
+		cmp := f(a[i], a[i+1])
+		if cmp > 0 {
+			increasing = false
+		}
+		if cmp < 0 {
+			decreasing = false
+		}
+	}
+	return increasing || decreasing
+
+}

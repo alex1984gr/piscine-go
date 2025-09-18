0062f59c167f9383af44a0e12b580717a05a39fe
agaliand <alexandrosgaliandras@yahoo.com>
2025-09-19 00:08:09 +0300

    shoppinglistsort.go

---
+++ shoppinglistsort.go
@@ -0,0 +1,14 @@
+package student
+
+func ShoppingListSort(slice []string) []string {
+	for i := 0; i <= len(slice)-1; i++ {
+		for j := i + 1; j < len(slice); j++ {
+			if len(slice[i]) > len(slice[j]) {
+				temp := slice[i]
+				slice[i] = slice[j]
+				slice[j] = temp
+			}
+		}
+	}
+	return slice
+}

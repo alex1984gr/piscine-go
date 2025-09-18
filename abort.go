0cb8ca0c4107cff8c4b369bfba8f2eec51e443b4
agaliand <alexandrosgaliandras@yahoo.com>
2025-09-18 15:58:28 +0300

    abort.go

--- abort.go
+++ abort.go
@@ -1,13 +1,15 @@
 package student
 
 func Abort(a, b, c, d, e int) int {
-	nums := [int]{a, b, c, d, e}
-	for i:= 0; i < len(nums) i++{
-		for j := 0; j < len(nums)-1; j++{
-			if nums[j], nums[j+1] {
-				nums[j], nums[j+1] = nums[j+1] , nums[j]
+	nums := []int{a, b, c, d, e}
+
+	for i := 0; i < len(nums); i++ {
+		for j := 0; j < len(nums)-1; j++ {
+			if nums[j] > nums[j+1] {
+				nums[j], nums[j+1] = nums[j+1], nums[j]
 			}
 		}
 	}
+
 	return nums[2]
 }

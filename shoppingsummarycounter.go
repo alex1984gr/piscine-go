a86219dd2ba0d0792eb09b1536bacbcd53fb4f3b
agaliand <alexandrosgaliandras@yahoo.com>
2025-09-18 16:56:38 +0300

    shoppingsummarycounter.go

--- shoppingsummarycounter.go
+++ shoppingsummarycounter.go
@@ -1,14 +1,19 @@
 package student
 
-import "strings"
-
 func ShoppingSummaryCounter(str string) map[string]int {
 	summary := map[string]int{}
-	words := strings.Split(str, " ")
+	word := ""
 
-	for _, word := range words {
-		summary[word] = summary[word] + 1
+	for _, ch := range str {
+		if ch != ' ' {
+			word += string(ch)
+		} else if word != "" {
+			summary[word] = summary[word] + 1
+			word = ""
+		}
+		if word != "" {
+			summary[word] = summary[word] + 1
+		}
 	}
-
 	return summary
 }

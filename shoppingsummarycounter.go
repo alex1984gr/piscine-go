8e715c5358a591c4de7650fa019410cf5be29a6a
agaliand <alexandrosgaliandras@yahoo.com>
2025-09-18 17:04:21 +0300

    shoppingsummarycounter.go

--- shoppingsummarycounter.go
+++ shoppingsummarycounter.go
@@ -7,13 +7,16 @@ func ShoppingSummaryCounter(str string) map[string]int {
 	for _, ch := range str {
 		if ch != ' ' {
 			word += string(ch)
-		} else if word != "" {
-			summary[word] = summary[word] + 1
-			word = ""
+		} else {
+			if word != "" {
+				summary[word] = summary[word] + 1
+				word = ""
+			}
 		}
 	}
 	if word != "" {
 		summary[word] = summary[word] + 1
 	}
+
 	return summary
 }

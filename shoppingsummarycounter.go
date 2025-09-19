1e62af3b566f23d29817742749858d72bab39e99
ALEXANDROS GALIANDRAS <alexandrosgaliandras@yahoo.com>
2025-09-19 17:23:01 +0300

    shoppingsummarycounter.go

--- shoppingsummarycounter.go
+++ shoppingsummarycounter.go
@@ -1,14 +1,20 @@
 package student
 
-import "strings"
-
 func ShoppingSummaryCounter(str string) map[string]int {
 	result := make(map[string]int)
-	items := strings.Split(str, " ")
-
-	for _, item := range items {
-		result[item]++
+	word := ""
+	for i := 0; i < len(str); i++ {
+		if str[i] != ' ' {
+			word += string(str[i])
+		} else {
+			if word != "" {
+				result[word]++
+				word = ""
+			}
+		}
 	}
-
+	if word != "" {
+		result[word]++
+	}
 	return result
 }

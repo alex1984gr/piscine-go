738521a697f1b6c15eb926cac290ade44d067c08
ALEXANDROS GALIANDRAS <alexandrosgaliandras@yahoo.com>
2025-09-19 10:49:30 +0300

    dealapackofcards.go

--- dealapackofcards.go
+++ dealapackofcards.go
@@ -3,23 +3,24 @@ package student
 import "github.com/01-edu/z01"
 
 func DealAPackOfCards(deck []int) {
-	players := 4
-	cardsPerPlayer := len(deck) / players
-
-	for i := 0; i < players; i++ {
-		playerHeader := "Player " + string('1'+i) + ": "
-		for _, r := range playerHeader {
-			z01.PrintRune(r)
-		}
-		for j := 0; j < cardsPerPlayer; j++ {
-			card := deck[i*cardsPerPlayer+j]
-			if card >= 10 {
-				z01.PrintRune(rune('0' + card/10))
-				z01.PrintRune(rune('0' + card%10))
-			} else {
-				z01.PrintRune(rune('0' + card))
+	for player := 0; player < 4; player++ {
+		z01.PrintRune('P')
+		z01.PrintRune('l')
+		z01.PrintRune('a')
+		z01.PrintRune('y')
+		z01.PrintRune('e')
+		z01.PrintRune('r')
+		z01.PrintRune(' ')
+		z01.PrintRune(rune('1' + player))
+		z01.PrintRune(':')
+		z01.PrintRune(' ')
+		for card := 0; card < 3; card++ {
+			num := deck[player*3+card]
+			if num >= 10 {
+				z01.PrintRune(rune('0' + num/10))
 			}
-			if j != cardsPerPlayer-1 {
+			z01.PrintRune(rune('0' + num%10))
+			if card < 2 {
 				z01.PrintRune(',')
 				z01.PrintRune(' ')
 			}

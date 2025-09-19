bf0679507621adf59b826378a92253da07543409
ALEXANDROS GALIANDRAS <alexandrosgaliandras@yahoo.com>
2025-09-19 10:42:21 +0300

    dealapackofcards.go

--- dealapackofcards.go
+++ dealapackofcards.go
@@ -2,11 +2,11 @@ package student
 
 import "github.com/01-edu/z01"
 
-func dealAPackOfCards(deck []int) {
+func DealAPackOfCards(deck [12]int) {
 	players := 4
-	cardsPerPlayer := len(deck) / players
-
-	for i := 0; i < players; i++ {
+	cardsPerPlayer := 3
+	index := 0
+	for p := 1; p <= players; p++ {
 		z01.PrintRune('P')
 		z01.PrintRune('l')
 		z01.PrintRune('a')
@@ -14,26 +14,32 @@ func dealAPackOfCards(deck []int) {
 		z01.PrintRune('e')
 		z01.PrintRune('r')
 		z01.PrintRune(' ')
-		z01.PrintRune(rune('1' + i))
+		z01.PrintRune(byte('0' + p))
 		z01.PrintRune(':')
 		z01.PrintRune(' ')
 
-		for j := 0; j < cardsPerPlayer; j++ {
-			card := deck[i*cardsPerPlayer+j]
-
-			if card >= 10 {
-				z01.PrintRune(rune('0' + card/10))
-				z01.PrintRune(rune('0' + card%10))
+		for c := 0; c < cardsPerPlayer; c++ {
+			card := deck[index]
+			if card < 10 {
+				z01.PrintRune(byte('0' + card))
 			} else {
-				z01.PrintRune(rune('0' + card))
+				if card == 10 {
+					z01.PrintRune('1')
+					z01.PrintRune('0')
+				} else if card == 11 {
+					z01.PrintRune('1')
+					z01.PrintRune('1')
+				} else if card == 12 {
+					z01.PrintRune('1')
+					z01.PrintRune('2')
+				}
 			}
-
-			if j != cardsPerPlayer-1 {
+			index++
+			if c < cardsPerPlayer-1 {
 				z01.PrintRune(',')
 				z01.PrintRune(' ')
 			}
 		}
-
 		z01.PrintRune('\n')
 	}
 }

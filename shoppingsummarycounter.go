19a7c795c7d76a8228531437b83d04df1044009c
ALEXANDROS GALIANDRAS <alexandrosgaliandras@yahoo.com>
2025-09-19 17:48:54 +0300

    shoppingsummarycounter.go

--- shoppingsummarycounter.go
+++ shoppingsummarycounter.go
@@ -4,23 +4,19 @@ func ShoppingSummaryCounter(str string) map[string]int {
 	result := make(map[string]int)
 	word := ""
 
-	for i := 0; i < len(str); i++ {
-		c := str[i]
-
-		if c == ' ' {
+	for i, r := range str {
+		if r == ' ' {
 			if word != "" {
 				result[word]++
 				word = ""
 			}
 		} else {
-			word += string(c)
+			word += string(r)
+		}
+		// Αν είμαστε στο τέλος της συμβολοσειράς
+		if i == len(str)-1 && word != "" {
+			result[word]++
 		}
 	}
-
-	// το τελευταίο word αν υπάρχει
-	if word != "" {
-		result[word]++
-	}
-
 	return result
 }

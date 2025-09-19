e27f7f64eb5a5d86d4ebc2ce5a8247b55e544351
ALEXANDROS GALIANDRAS <alexandrosgaliandras@yahoo.com>
2025-09-19 11:00:39 +0300

    dealapackofcards.go

--- dealapackofcards.go
+++ dealapackofcards.go
@@ -3,24 +3,28 @@ package student
 import "github.com/01-edu/z01"
 
 func DealAPackOfCards(deck []int) {
+	// Παίκτες
 	players := []string{"Player 1: ", "Player 2: ", "Player 3: ", "Player 4: "}
-	cardStrings := [][]rune{
+
+	// Προκαθορισμένες κάρτες σαν runes, αποφεύγοντας μετατροπές
+	cards := [][]rune{
 		{'1'}, {'2'}, {'3'}, {'4'}, {'5'}, {'6'},
 		{'7'}, {'8'}, {'9'}, {'1', '0'}, {'1', '1'}, {'1', '2'},
 	}
 
-	cardIndex := 0
+	index := 0
+
 	for i := 0; i < 4; i++ {
 		// Όνομα παίκτη
 		for _, r := range players[i] {
 			z01.PrintRune(r)
 		}
-		// 3 κάρτες
+		// 3 κάρτες για τον κάθε παίκτη
 		for j := 0; j < 3; j++ {
-			for _, r := range cardStrings[cardIndex] {
+			for _, r := range cards[index] {
 				z01.PrintRune(r)
 			}
-			cardIndex++
+			index++
 			if j < 2 {
 				z01.PrintRune(',')
 				z01.PrintRune(' ')

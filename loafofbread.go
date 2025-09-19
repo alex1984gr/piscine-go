43df28d5e7b95398de2b2a1256604b3adc76ba52
agaliand <alexandrosgaliandras@yahoo.com>
2025-09-19 09:19:53 +0300

    loafofbread.go

--- loafofbread.go
+++ loafofbread.go
@@ -4,6 +4,21 @@ import "github.com/01-edu/z01"
 
 func LoafOfBread(str string) string {
 	if len(str) < 5 {
+		z01.PrintRune('I')
+		z01.PrintRune('n')
+		z01.PrintRune('v')
+		z01.PrintRune('a')
+		z01.PrintRune('l')
+		z01.PrintRune('i')
+		z01.PrintRune('d')
+		z01.PrintRune(' ')
+		z01.PrintRune('O')
+		z01.PrintRune('u')
+		z01.PrintRune('t')
+		z01.PrintRune('p')
+		z01.PrintRune('u')
+		z01.PrintRune('t')
+		z01.PrintRune('\n')
 		return "Invalid Output\n"
 	}
 
@@ -16,15 +31,12 @@ func LoafOfBread(str string) string {
 			result += string(ch)
 			continue
 		}
-
 		if skipNext {
 			skipNext = false
 			continue
 		}
-
 		result += string(ch)
 		count++
-
 		if count == 5 {
 			count = 0
 			skipNext = true

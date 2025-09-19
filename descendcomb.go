92f192e379f079f7e9251245f8110896ac2c3946
agaliand <alexandrosgaliandras@yahoo.com>
2025-09-19 08:48:18 +0300

    descendcomb.go

---
+++ descendcomb.go
@@ -0,0 +1,25 @@
+package student
+
+import "github.com/01-edu/z01"
+
+func printNumber(n int) {
+	z01.PrintRune(rune(n/10 + '0'))
+	z01.PrintRune(rune(n%10 + '0'))
+}
+
+func DescendComb() {
+	first := true
+	for i := 99; i >= 0; i-- {
+		for j := i - 1; j >= 0; j-- {
+			if !first {
+				z01.PrintRune(',')
+				z01.PrintRune(' ')
+			}
+			printNumber(i)
+			z01.PrintRune(' ')
+			printNumber(j)
+			first = false
+		}
+	}
+	z01.PrintRune('\n')
+}

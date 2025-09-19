90ffa411a3c02fdbde09a376a3c5f7fb5e2c0028
ALEXANDROS GALIANDRAS <alexandrosgaliandras@yahoo.com>
2025-09-19 11:15:48 +0300

    dealapackofcards.go

--- dealapackofcards.go
+++ dealapackofcards.go
@@ -1,45 +1,17 @@
 package student
 
-import "github.com/01-edu/z01"
+import "fmt"
 
 func DealAPackOfCards(deck []int) {
 	for i := 0; i < 4; i++ {
-		printString("Player ")
-		printInt(i + 1)
-		printString(": ")
-
+		fmt.Printf("Player %d: ", i+1)
 		for j := 0; j < 13; j++ {
 			index := i*13 + j
-			printInt(deck[index])
+			fmt.Print(deck[index])
 			if j < 12 {
-				printString(", ")
+				fmt.Print(", ")
 			}
 		}
-		z01.PrintRune('\n')
-	}
-}
-
-// Εκτυπώνει έναν ακέραιο αριθμό με z01.PrintRune
-func printInt(n int) {
-	if n == 0 {
-		z01.PrintRune('0')
-		return
-	}
-
-	digits := []rune{}
-	for n > 0 {
-		digits = append([]rune{rune(n%10 + '0')}, digits...)
-		n /= 10
-	}
-
-	for _, d := range digits {
-		z01.PrintRune(d)
-	}
-}
-
-// Εκτυπώνει string χαρακτήρα-χαρακτήρα
-func printString(s string) {
-	for _, ch := range s {
-		z01.PrintRune(ch)
+		fmt.Println()
 	}
 }

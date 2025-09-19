4a1cc2257e20485d329604b66bf9680dd8699beb
ALEXANDROS GALIANDRAS <alexandrosgaliandras@yahoo.com>
2025-09-19 10:38:56 +0300

    dealapackofcards.go

--- dealapackofcards.go
+++ dealapackofcards.go
@@ -2,25 +2,34 @@ package student
 
 import "github.com/01-edu/z01"
 
-func DealAPackOfCards(deck []int) {
-	for i := 0; i < 4; i++ {
-		player := "Player " + string('1'+i) + ": "
-		for _, r := range player {
-			z01.PrintRune(r)
-		}
-		for j := 0; j < 3; j++ {
-			card := deck[i*3+j]
+func dealAPackOfCards(deck []int) {
+	players := 4
+	cardsPerPlayer := len(deck) / players
+	for i := 0; i < players; i++ {
+		z01.PrintRune('P')
+		z01.PrintRune('l')
+		z01.PrintRune('a')
+		z01.PrintRune('y')
+		z01.PrintRune('e')
+		z01.PrintRune('r')
+		z01.PrintRune(' ')
+		z01.PrintRune(rune('1' + i))
+		z01.PrintRune(':')
+		z01.PrintRune(' ')
+		for j := 0; j < cardsPerPlayer; j++ {
+			card := deck[i*cardsPerPlayer+j]
 			if card >= 10 {
 				z01.PrintRune(rune('0' + card/10))
 				z01.PrintRune(rune('0' + card%10))
 			} else {
 				z01.PrintRune(rune('0' + card))
 			}
-			if j < 2 {
+			if j != cardsPerPlayer-1 {
 				z01.PrintRune(',')
 				z01.PrintRune(' ')
 			}
 		}
 		z01.PrintRune('\n')
 	}
+
 }

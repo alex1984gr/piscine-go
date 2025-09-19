c6781eca8c98e1a19da836cad89169a7f2d88767
ALEXANDROS GALIANDRAS <alexandrosgaliandras@yahoo.com>
2025-09-19 17:35:39 +0300

    shoppingsummarycounter.go

--- shoppingsummarycounter.go
+++ shoppingsummarycounter.go
@@ -1,29 +1,14 @@
 package student
 
-func printStr(s string) {
-	for i := 0; i < len(s); i++ {
-		PrintRune(rune(s[i])) // Χωρίς z01.
-	}
-}
+import "strings"
 
-func printNum(n int) {
-	if n == 0 {
-		PrintRune('0')
-		return
+func ShoppingSummaryCounter(str string) map[string]int {
+	result := make(map[string]int)
+	words := strings.Fields(str) // σπάει το string σε slice με βάση τα κενά
+
+	for _, word := range words {
+		result[word]++
 	}
 
-	if n < 0 {
-		PrintRune('-')
-		n = -n
-	}
-
-	var digits []rune
-	for n > 0 {
-		digits = append(digits, rune(n%10)+'0')
-		n /= 10
-	}
-
-	for i := len(digits) - 1; i >= 0; i-- {
-		PrintRune(digits[i])
-	}
+	return result
 }

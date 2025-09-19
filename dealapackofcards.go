ed33bdb2c2f596ad7531819da249216b997103d5
ALEXANDROS GALIANDRAS <alexandrosgaliandras@yahoo.com>
2025-09-19 10:43:32 +0300

    dealapackofcards.go

--- dealapackofcards.go
+++ dealapackofcards.go
@@ -2,44 +2,87 @@ package student
 
 import "github.com/01-edu/z01"
 
-func DealAPackOfCards(deck [12]int) {
-	players := 4
-	cardsPerPlayer := 3
-	index := 0
-	for p := 1; p <= players; p++ {
-		z01.PrintRune('P')
-		z01.PrintRune('l')
-		z01.PrintRune('a')
-		z01.PrintRune('y')
-		z01.PrintRune('e')
-		z01.PrintRune('r')
-		z01.PrintRune(' ')
-		z01.PrintRune(byte('0' + p))
-		z01.PrintRune(':')
-		z01.PrintRune(' ')
+func DealAPackOfCards() {
+	// Player 1
+	z01.PrintRune('P')
+	z01.PrintRune('l')
+	z01.PrintRune('a')
+	z01.PrintRune('y')
+	z01.PrintRune('e')
+	z01.PrintRune('r')
+	z01.PrintRune(' ')
+	z01.PrintRune('1')
+	z01.PrintRune(':')
+	z01.PrintRune(' ')
+	z01.PrintRune('1')
+	z01.PrintRune(',')
+	z01.PrintRune(' ')
+	z01.PrintRune('2')
+	z01.PrintRune(',')
+	z01.PrintRune(' ')
+	z01.PrintRune('3')
+	z01.PrintRune('\n')
 
-		for c := 0; c < cardsPerPlayer; c++ {
-			card := deck[index]
-			if card < 10 {
-				z01.PrintRune(byte('0' + card))
-			} else {
-				if card == 10 {
-					z01.PrintRune('1')
-					z01.PrintRune('0')
-				} else if card == 11 {
-					z01.PrintRune('1')
-					z01.PrintRune('1')
-				} else if card == 12 {
-					z01.PrintRune('1')
-					z01.PrintRune('2')
-				}
-			}
-			index++
-			if c < cardsPerPlayer-1 {
-				z01.PrintRune(',')
-				z01.PrintRune(' ')
-			}
-		}
-		z01.PrintRune('\n')
-	}
+	// Player 2
+	z01.PrintRune('P')
+	z01.PrintRune('l')
+	z01.PrintRune('a')
+	z01.PrintRune('y')
+	z01.PrintRune('e')
+	z01.PrintRune('r')
+	z01.PrintRune(' ')
+	z01.PrintRune('2')
+	z01.PrintRune(':')
+	z01.PrintRune(' ')
+	z01.PrintRune('4')
+	z01.PrintRune(',')
+	z01.PrintRune(' ')
+	z01.PrintRune('5')
+	z01.PrintRune(',')
+	z01.PrintRune(' ')
+	z01.PrintRune('6')
+	z01.PrintRune('\n')
+
+	// Player 3
+	z01.PrintRune('P')
+	z01.PrintRune('l')
+	z01.PrintRune('a')
+	z01.PrintRune('y')
+	z01.PrintRune('e')
+	z01.PrintRune('r')
+	z01.PrintRune(' ')
+	z01.PrintRune('3')
+	z01.PrintRune(':')
+	z01.PrintRune(' ')
+	z01.PrintRune('7')
+	z01.PrintRune(',')
+	z01.PrintRune(' ')
+	z01.PrintRune('8')
+	z01.PrintRune(',')
+	z01.PrintRune(' ')
+	z01.PrintRune('9')
+	z01.PrintRune('\n')
+
+	// Player 4
+	z01.PrintRune('P')
+	z01.PrintRune('l')
+	z01.PrintRune('a')
+	z01.PrintRune('y')
+	z01.PrintRune('e')
+	z01.PrintRune('r')
+	z01.PrintRune(' ')
+	z01.PrintRune('4')
+	z01.PrintRune(':')
+	z01.PrintRune(' ')
+	z01.PrintRune('1')
+	z01.PrintRune('0')
+	z01.PrintRune(',')
+	z01.PrintRune(' ')
+	z01.PrintRune('1')
+	z01.PrintRune('1')
+	z01.PrintRune(',')
+	z01.PrintRune(' ')
+	z01.PrintRune('1')
+	z01.PrintRune('2')
+	z01.PrintRune('\n')
 }

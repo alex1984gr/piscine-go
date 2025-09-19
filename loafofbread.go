4e1b6326352b723dce317cf17115681a198a217a
agaliand <alexandrosgaliandras@yahoo.com>
2025-09-19 09:08:27 +0300

    loafofbread.go

--- loafofbread.go
+++ loafofbread.go
@@ -1,32 +1,32 @@
 package student
 
+import "github.com/01-edu/z01"
+
 func LoafOfBread(str string) string {
 	if len(str) < 5 {
 		return "Invalid Output\n"
 	}
-	result := ""
-	temp := ""
+
 	count := 0
 	skipNext := false
+
 	for _, ch := range str {
 		if skipNext {
 			skipNext = false
 			continue
 		}
+
 		if ch != ' ' {
-			temp += string(ch)
+			z01.PrintRune(ch)
 			count++
 		}
+
 		if count == 5 {
-			result += temp
-			temp = ""
 			count = 0
 			skipNext = true
 		}
 	}
-	if temp != "" {
-		result += temp
-	}
 
-	return result + "\n"
+	z01.PrintRune('\n')
+	return ""
 }

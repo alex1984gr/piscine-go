ac703623b95efcce691b067a151017ef7c7124d6
ALEXANDROS GALIANDRAS <alexandrosgaliandras@yahoo.com>
2025-09-19 11:08:15 +0300

    dealapackofcards.go

--- dealapackofcards.go
+++ dealapackofcards.go
@@ -3,28 +3,25 @@ package student
 import "github.com/01-edu/z01"
 
 func DealAPackOfCards(deck []int) {
-	// Παίκτες
-	players := []string{"Player 1: ", "Player 2: ", "Player 3: ", "Player 4: "}
-
-	// Προκαθορισμένες κάρτες σαν runes, αποφεύγοντας μετατροπές
-	cards := [][]rune{
-		{'1'}, {'2'}, {'3'}, {'4'}, {'5'}, {'6'},
-		{'7'}, {'8'}, {'9'}, {'1', '0'}, {'1', '1'}, {'1', '2'},
+	playerNames := [][]int{
+		{'P', 'l', 'a', 'y', 'e', 'r', ' ', 1, ':', ' '},
+		{'P', 'l', 'a', 'y', 'e', 'r', ' ', 2, ':', ' '},
+		{'P', 'l', 'a', 'y', 'e', 'r', ' ', 3, ':', ' '},
+		{'P', 'l', 'a', 'y', 'e', 'r', ' ', 4, ':', ' '},
 	}
 
-	index := 0
-
 	for i := 0; i < 4; i++ {
-		// Όνομα παίκτη
-		for _, r := range players[i] {
-			z01.PrintRune(r)
+		for _, c := range playerNames[i] {
+			z01.PrintRune(c)
 		}
-		// 3 κάρτες για τον κάθε παίκτη
 		for j := 0; j < 3; j++ {
-			for _, r := range cards[index] {
-				z01.PrintRune(r)
+			val := deck[i*3+j]
+			if val >= 10 {
+				z01.PrintRune(1 + '0')
+				z01.PrintRune(val%10 + '0')
+			} else {
+				z01.PrintRune(val + '0')
 			}
-			index++
 			if j < 2 {
 				z01.PrintRune(',')
 				z01.PrintRune(' ')

a8817b55fabfe54f29c8dc356fa1d7d7c50b91a5
ALEXANDROS GALIANDRAS <alexandrosgaliandras@yahoo.com>
2025-09-19 17:25:45 +0300

    shoppingsummarycounter.go

--- shoppingsummarycounter.go
+++ shoppingsummarycounter.go
@@ -3,6 +3,7 @@ package student
 func ShoppingSummaryCounter(str string) map[string]int {
 	result := make(map[string]int)
 	word := ""
+
 	for i := 0; i < len(str); i++ {
 		if str[i] != ' ' {
 			word += string(str[i])

097bfbf635bfbc7175bbf57a8ee73451273ddcfb
ALEXANDROS GALIANDRAS <alexandrosgaliandras@yahoo.com>
2025-09-19 17:58:47 +0300

    dealapackofcards.go

--- dealapackofcards.go
+++ dealapackofcards.go
@@ -8,14 +8,13 @@ import (
 
 func DealAPackOfCards(deck []int) {
 	for i := 0; i < 4; i++ {
-		// εκτυπώνουμε το prefix "Player X: "
+		// prefix μόνο με Printf (επιτρέπεται)
 		fmt.Printf("Player %d: ", i+1)
 
-		// εκτυπώνουμε τις 3 κάρτες του παίκτη
 		for j := 0; j < 3; j++ {
 			val := deck[i*3+j]
 
-			// ειδική περίπτωση για 10, 11, 12
+			// χωρίς rune cast, explicit για κάθε περίπτωση
 			if val == 10 {
 				z01.PrintRune('1')
 				z01.PrintRune('0')
@@ -25,19 +24,31 @@ func DealAPackOfCards(deck []int) {
 			} else if val == 12 {
 				z01.PrintRune('1')
 				z01.PrintRune('2')
-			} else {
-				// μονοψήφιο
-				z01.PrintRune('0' + rune(val))
+			} else if val == 1 {
+				z01.PrintRune('1')
+			} else if val == 2 {
+				z01.PrintRune('2')
+			} else if val == 3 {
+				z01.PrintRune('3')
+			} else if val == 4 {
+				z01.PrintRune('4')
+			} else if val == 5 {
+				z01.PrintRune('5')
+			} else if val == 6 {
+				z01.PrintRune('6')
+			} else if val == 7 {
+				z01.PrintRune('7')
+			} else if val == 8 {
+				z01.PrintRune('8')
+			} else if val == 9 {
+				z01.PrintRune('9')
 			}
 
-			// κόμμα και κενό, εκτός από την τελευταία κάρτα
 			if j < 2 {
 				z01.PrintRune(',')
 				z01.PrintRune(' ')
 			}
 		}
-
-		// newline στο τέλος κάθε παίκτη
 		z01.PrintRune('\n')
 	}
 }

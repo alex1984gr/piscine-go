7cdc084edb00b1ab86d0c9aaa809c1f03b5f99fc
ALEXANDROS GALIANDRAS <alexandrosgaliandras@yahoo.com>
2025-09-19 10:40:11 +0300

    dealapackofcards.go

--- dealapackofcards.go
+++ dealapackofcards.go
@@ -5,6 +5,7 @@ import "github.com/01-edu/z01"
 func dealAPackOfCards(deck []int) {
 	players := 4
 	cardsPerPlayer := len(deck) / players
+
 	for i := 0; i < players; i++ {
 		z01.PrintRune('P')
 		z01.PrintRune('l')
@@ -16,20 +17,23 @@ func dealAPackOfCards(deck []int) {
 		z01.PrintRune(rune('1' + i))
 		z01.PrintRune(':')
 		z01.PrintRune(' ')
+
 		for j := 0; j < cardsPerPlayer; j++ {
 			card := deck[i*cardsPerPlayer+j]
+
 			if card >= 10 {
 				z01.PrintRune(rune('0' + card/10))
 				z01.PrintRune(rune('0' + card%10))
 			} else {
 				z01.PrintRune(rune('0' + card))
 			}
+
 			if j != cardsPerPlayer-1 {
 				z01.PrintRune(',')
 				z01.PrintRune(' ')
 			}
 		}
+
 		z01.PrintRune('\n')
 	}
-
 }

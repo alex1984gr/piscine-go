84336652d23818337e83932ee61a6e2a93cf83eb
ALEXANDROS GALIANDRAS <alexandrosgaliandras@yahoo.com>
2025-09-24 17:13:36 +0300

    listpushfront.go

---
+++ listpushfront.go
@@ -0,0 +1,20 @@
+package student 
+
+type NodeL struct{
+	Data interface
+	Next *NodeL
+}
+type List struct{
+	Head *NodeL
+	Tail *NodeL
+}
+func ListPushFront(l *List, data interface{}) {
+	newNode :=&Nodel{Data: data}
+	if l.Head == nil{
+		l.Head = newNode
+		l.Tail = newNode
+	}else {
+		newNode.Next = l.Head
+		l.Head =newNode
+	}
+} 

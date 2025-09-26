010edae07f3ca63ea78713caabac8400adcf1538
ALEXANDROS GALIANDRAS <alexandrosgaliandras@yahoo.com>
2025-09-26 06:30:54 +0300

    btreeisbinary.go

---
+++ btreeisbinary.go
@@ -0,0 +1,17 @@
+package student
+
+func BTreeIsBinary(root *TreeNode) bool {
+	return helper(root, "", "")
+}
+func helper(node *TreeNode, minValeu, maxValeu string) bool {
+	if node == nil {
+		return true
+	}
+	if minValeu != "" && node.Data <= minValeu {
+		return false
+	}
+	if maxValeu != "" && node.Data >= maxValeu {
+		return false
+	}
+	return helper(node.Left, minValeu, node.Data) && helper(node.Right, node.Data, maxValeu)
+}

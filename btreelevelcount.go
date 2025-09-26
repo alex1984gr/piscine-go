97b08893547d035c020414111fdd8a023c39fb85
ALEXANDROS GALIANDRAS <alexandrosgaliandras@yahoo.com>
2025-09-26 06:09:25 +0300

    btreelevelcount.go

---
+++ btreelevelcount.go
@@ -0,0 +1,16 @@
+package student
+
+func BTreeLevelCount(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	leftHeight := BTreeLevelCount(root.Left)
+	rightHeight := BTreeLevelCount(root.Right)
+	return max(leftHeight, rightHeight) + 1
+}
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}

12fc2dda1f086633e5e6d811e2fc23e35024afa9
ALEXANDROS GALIANDRAS <alexandrosgaliandras@yahoo.com>
2025-09-26 05:41:12 +0300

    btreesearchitem.go

---
+++ btreesearchitem.go
@@ -0,0 +1,15 @@
+package student
+
+func BTreeSearchItem(root *TreeNode, elem string) *TreeNode {
+	if root == nil {
+		return nil
+	}
+	if root.Data == elem {
+		return root
+	}
+	if elem < root.Data {
+		return BTreeSearchItem(root.Left, elem)
+	} else {
+		return BTreeSearchItem(root.Right, elem)
+	}
+}

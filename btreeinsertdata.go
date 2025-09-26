0ebfafe05d2cce40f5e8a71a073cc42f78062692
ALEXANDROS GALIANDRAS <alexandrosgaliandras@yahoo.com>
2025-09-26 05:06:04 +0300

    btreeinsertdata.go

---
+++ btreeinsertdata.go
@@ -0,0 +1,29 @@
+package student
+
+type TreeNode struct {
+	Left, Right, Parent *TreeNode
+	Data                string
+}
+
+func BTreeInsertData(root *TreeNode, data string) *TreeNode {
+	if root == nil {
+		return &TreeNode{Data: data}
+	}
+	current := root
+	for {
+		if Data < current.Data {
+			if current.Left == nil {
+				current.Left = &TreeNode{Data: data, Parent: current}
+				break
+			}
+			current = current.Left
+		} else {
+			if current.Right == nil {
+				current.Right = &TreeNode{Data: data, Parent: current}
+				break
+			}
+			current = current.Right
+		}
+	}
+	return root
+}

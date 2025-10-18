896493be64016923487a486f8bb1f09d58ad2ac1
ALEXANDROS GALIANDRAS <alexandrosgaliandras@yahoo.com>
2025-10-19 00:37:17 +0300

    main.go

--- go-reloaded/main.go
+++ go-reloaded/main.go
@@ -1,10 +1,17 @@
 package main
 
+import "fmt"
+
 func main() {
-	input := raedInput()
-	tokens := tokenize(input)
-	afterTag := FilterAfterTag(tokens)
-	InsideQuotes := ExtractQuotes(afterTag)
-	vowelWords := FilterVowelWords(InsideQuotes)
-	PrintOutput(vowelWords)
+	fmt.Println("Enter text:") // μήνυμα προς χρήστη
+	input := ReadInput()       // διαβάζει όλο το input
+
+	tokens := tokenize(input)              // σπάει σε tokens
+	afterTag := filterAfterTag(tokens)     // κρατά μόνο tokens μετά από tag
+	quotes := extractQuotes(afterTag)      // κρατά μόνο tokens μέσα σε εισαγωγικά
+	vowelWords := filterVowelWords(quotes) // φιλτράρει λέξεις
+
+	// Εκτυπώνουμε το αποτέλεσμα για να χρησιμοποιήσουμε τη μεταβλητή
+	fmt.Println("Words that start with vowels:")
+	printOutput(vowelWords)
 }
